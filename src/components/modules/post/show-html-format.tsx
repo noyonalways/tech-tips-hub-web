8c84f20eaeea96af6e1ef468733aa1def162a226
noyonalways <noyonrahman2003@gmail.com>
2024-11-06 20:08:09 +0600

    fix: blog details page rich text issue

--- src/components/modules/post/show-html-format.tsx
+++ src/components/modules/post/show-html-format.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false }) as any;
 
@@ -11,10 +12,14 @@ interface IProps {
 const ShowHTMLFormat = ({ content }: IProps) => {
   return (
     <ReactQuill
-      readOnly
-      modules={{ toolbar: false }}
-      theme="snow"
+      className="mb-4"
       value={content}
+      required
+      placeholder="Write your blog here..."
+      modules={{
+        toolbar: false,
+      }}
+      theme="snow"
     />
   );
 };
