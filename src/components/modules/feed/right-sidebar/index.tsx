abd4cb0d5c3d306a5341aeda474c7bfca46f3a56
noyonalways <noyonrahman2003@gmail.com>
2024-11-27 00:26:37 +0600

    fix: right side data isssue

--- src/components/modules/feed/right-sidebar/index.tsx
+++ src/components/modules/feed/right-sidebar/index.tsx
@@ -1,23 +1,48 @@
+"use client"
+
+import React, { useEffect, useState } from "react";
 import MiniFooter from "@/components/shared/mini-footer";
-import CategoryButton from "@/components/ui/category-button";
+import { Skeleton } from "@nextui-org/skeleton";
+import Categories from "./categories";
 import { getAllCategories } from "@/services/category";
 import { ICategory } from "@/types";
 
-interface IProps {}
+const RightSideBar = () => {
+  const [categories, setCategories] = useState<ICategory[] | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-const RightSideBar = async ({}: IProps) => {
-  const categoryData = await getAllCategories();
-  const categories = categoryData?.data as ICategory[];
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const categoryData = await getAllCategories();
+        setCategories(categoryData?.data || []);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
   return (
-    <div className="hidden lg:inline-block basis-[18%] space-y-6 sticky top-16  lg:p-4 lg:pt-4">
-      <div className="border border-default/50 p-6 rounded-xl space-y-2">
-        <h2 className="font-semibold text-lg">Categories</h2>
-        <div className="gap-2 flex flex-wrap">
-          {categories?.map((category) => (
-            <CategoryButton key={category._id} {...category} />
-          ))}
+    <div className="hidden lg:inline-block basis-[18%] space-y-6 sticky top-16 lg:p-4 lg:pt-4">
+      {isLoading ? (
+        <div className="border border-default/50 p-6 rounded-xl space-y-2">
+          <h2 className="font-semibold text-lg">Categories</h2>
+          <div className="gap-2 flex flex-wrap">
+            <Skeleton className="h-8 w-20 rounded-full" />
+            <Skeleton className="h-8 w-24 rounded-full" />
+            <Skeleton className="h-8 w-16 rounded-full" />
+            <Skeleton className="h-8 w-16 rounded-full" />
+            <Skeleton className="h-8 w-16 rounded-full" />
+            <Skeleton className="h-8 w-20 rounded-full" />
+          </div>
         </div>
-      </div>
+      ) : (
+        <Categories categories={categories || []} />
+      )}
       <MiniFooter />
     </div>
   );
